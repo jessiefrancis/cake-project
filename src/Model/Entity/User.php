cb7ec92c1ff2c836b4be2ebd393ebb72086974e3
jessiefrancis <jessierealigue@gmail.com>
2021-08-09 21:41:03 +0800

    password hasher

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -31,4 +32,8 @@ class User extends Entity
         'CreatedAt' => true,
         'UpdatedAt' => true,
     ];
+
+    protected function _setPassword($password){
+        return (new DefaultPasswordHasher)->hash($password);
+    }
 }
